64fabaac322a1554437979e427752c760aad1b9f
konojunya <junya03dance@gmail.com>
2015-10-30 19:29:09 +0900

    microscope

---
+++ client/template/posts/post_submit.js
@@ -0,0 +1,17 @@
+Template.postSubmit.events({
+	'submit form': function(e){
+		e.preventDefault();
+
+		var date = new Date();
+		var time= date.getTime();
+		var post = {
+			url: $(e.target).find('[name=url]').val(),
+			title: $(e.target).find('[name=title]').val(),
+			message: $(e.target).find('[name=message]').val(),
+			timestamp: time
+		}
+
+		post._id = Posts.insert(post);
+		Router.go('postsList');
+	}
+});
